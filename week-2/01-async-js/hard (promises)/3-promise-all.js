065853c580e5775b2a058764dcd2f48ba41684ee
Pritam Chauhan <rajputpritam619@gmail.com>
2024-01-10 23:20:25 +0530

    some updates in problem 3 code

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,28 +5,36 @@
  */
 
 function wait1(t) {
-  return new Promise(function(resolve, reject){
-    setTimeout(resolve,t)
+  return new Promise((resolve, reject)=>{
+    setTimeout(function(){
+      resolve()
+    }, t*1000);
   })
 }
 
 function wait2(t) {
-    return new Promise(function(resolve, reject){
-        setTimeout(resolve,t)
-     })
+  return new Promise((resolve, reject)=>{
+    setTimeout(function(){
+      resolve()
+    }, t*1000);
+  })
 }
 
 function wait3(t) {
-    return new Promise(function(resolve, reject){
-        setTimeout(resolve,t)
-     })
+  return new Promise((resolve, reject)=>{
+    setTimeout(function(){
+      resolve()
+    }, t*1000);
+  })
 }
 
 function calculateTime(t1, t2, t3) {
-  const startPonit = Date.now();
-  Promise.all([wait1(t1), wait2(t2), wait3(t3)]);
-  const endPoint = Date.now();
-  return (endPoint - startPonit);
+  const startTime = Date.now();
+  return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then(()=>{
+    const endTime = Date.now();
+    return endTime - startTime;
+  })
 }
+
 
 module.exports = calculateTime;
